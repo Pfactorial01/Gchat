0fccd7c45d9e903ff4b04040b320990df2d2c475
Adebayo Adeoye <71315247+Pfactorial01@users.noreply.github.com>
2023-03-05 22:14:18 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express")
+const http = require('http')
+const path = require('path')
+const socketio = require('socket.io')
+const formatMessage = require("./utils/messages")
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require("./utils/users")
+
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server) 
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+const botName = `GeoChat Bot`
+
+// Run when a client connects
+io.on('connection', socket => {
+socket.on('joinRoom', ({username, room}) => {
+    const user = userJoin(socket.id, username, room)
+    socket.join(user.room)
+
+    socket.emit('message', formatMessage(botName,'Welcome to GeoChat'))
+
+    // Broadcast new user connection
+    socket.broadcast.to(user.room).emit('message', formatMessage(botName,`${username} joined the chat`))
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+    })
+
+})
+
+
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id)
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
+
+    // Broadcat client disconnection
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id)
+        if (user) {
+        io.to(user.room).emit('message', formatMessage(botName,`${user.username} has left the chat`))
+        
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+        }
+    })
+    })
+})
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
